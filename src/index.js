f926476e83973b3309ca003d1eb356e81299b207
Stepan Penyaz <stepan_penyaz@epam.com>
2020-02-27 18:12:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+  var resultString = '';
+  var tensNames = [
+    "",
+    "ten",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety"
+  ];
+
+  var numNames = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen"
+  ];
+
+
+  if (number == 0)
+    return numNames[number];
+
+  return convertNumber(resultString, number);
+
+  function convertNumber(result, passedNumber) {
+    if (passedNumber < 20) {
+      if (passedNumber == 0) {
+        return result.trim();
+      }
+      return (result + numNames[passedNumber]).trim();
+    }
+
+    if (passedNumber < 100) {
+      result += tensNames[(passedNumber / 10 | 0)];
+      return convertNumber(result + ' ', (passedNumber % 10));
+    }
+    else {
+      result += numNames[(passedNumber / 100 | 0)] + ' hundred';
+      return convertNumber(result + ' ', (passedNumber % 100));
+    }
+  }
 }
